cdc1c5ec309d4ebdb300ccc99373ac2050699f06
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-02-22 21:17:48 +0800

    add Rx

--- src/graphs/medial.jl
+++ src/graphs/medial.jl
@@ -87,3 +87,62 @@ function planar_rz()
 
     return PlanarGraph(vertices, faces_half_edges, half_edges_faces)
 end
+
+function planar_rx()
+    vertices = Dict{Vertex, HalfEdge}(
+        Vertex(1) => HalfEdge(1, 9),
+        Vertex(2) => HalfEdge(2, 1),
+        Vertex(3) => HalfEdge(3, 2),
+        Vertex(4) => HalfEdge(4, 3),
+        Vertex(5) => HalfEdge(5, 4),
+        Vertex(6) => HalfEdge(6, 5),
+        Vertex(7) => HalfEdge(7, 6),
+        Vertex(8) => HalfEdge(8, 7),
+        Vertex(9) => HalfEdge(9, 8),
+        Vertex(10) => HalfEdge(10, 2),
+        Vertex(11) => HalfEdge(11, 5),
+    )
+
+    faces_half_edges = Dict{Face, HalfEdge}(
+        Face(1) => HalfEdge(1, 8),
+        Face(2) => HalfEdge(2, 1),
+        Face(3) => HalfEdge(3, 2),
+        Face(4) => HalfEdge(7, 6),
+        Face(5) => HalfEdge(4, 3),
+        Face(6) => HalfEdge(5, 4),
+    )
+
+    half_edges_faces = Dict{HalfEdge, Face}(
+        HalfEdge(9, 1) => Face(1),
+        HalfEdge(1, 8) => Face(1),
+        HalfEdge(8, 9) => Face(1),
+
+        HalfEdge(1, 9) => Face(2),
+        HalfEdge(9, 8) => Face(2),
+        HalfEdge(8, 7) => Face(2),
+        HalfEdge(7, 10) => Face(2),
+        HalfEdge(10, 2) => Face(2),
+        HalfEdge(2, 1) => Face(2),
+
+        HalfEdge(2, 10) => Face(3),
+        HalfEdge(10, 3) => Face(3),
+        HalfEdge(3, 2) => Face(3),
+
+        HalfEdge(6, 10) => Face(4),
+        HalfEdge(10, 7) => Face(4),
+        HalfEdge(7, 6) => Face(4),
+
+        HalfEdge(3, 10) => Face(5),
+        HalfEdge(10, 6) => Face(5),
+        HalfEdge(6, 5) => Face(5),
+        HalfEdge(5, 11) => Face(5),
+        HalfEdge(11, 4) => Face(5),
+        HalfEdge(4, 3) => Face(5),
+
+        HalfEdge(4, 11) => Face(6),
+        HalfEdge(11, 5) => Face(6),
+        HalfEdge(5, 4) => Face(6),
+    )
+
+    return PlanarGraph(vertices, faces_half_edges, half_edges_faces)
+end
