4c4cb350b62a21e2dfcb7e5f4079fa17d2517074
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-03-11 16:20:26 +0800

    add tests for contract

--- test/contract.jl
+++ test/contract.jl
@@ -7,19 +7,22 @@ Ia = Vertex[9, 8, 7, 6, 5, 13]
 Ib = Vertex[9, 1, 2, 3, 4, 13]
 
 @testset "vertices_map" begin
-    map_a, map_b = vertices_map(a, b, Ia, Ib)
+    vmap_a, vmap_b = vertices_map(a, b, Ia, Ib)
     for (va, vb) in zip(Ia, Ib)
-        @test map_a[va] == map_b[vb]
+        @test vmap_a[va] == vmap_b[vb]
     end
 end
 
 @testset "faces_map" begin
-    map_a, map_b = faces_map(a, b, Ia, Ib)
-    @test map_a[Face(6)] == map_b[Face(1)]
-    @test map_a[Face(5)] == map_b[Face(2)]
-    @test map_a[Face(2)] == map_b[Face(3)]
-    @test map_a[Face(3)] == map_b[Face(4)]
-    @test map_a[Face(4)] == map_b[Face(5)]
+    fmap_a, fmap_b = faces_map(a, b, Ia, Ib)
+    @test fmap_a[Face(6)] == fmap_b[Face(1)]
+    @test fmap_a[Face(5)] == fmap_b[Face(2)]
+    @test fmap_a[Face(2)] == fmap_b[Face(3)]
+    @test fmap_a[Face(3)] == fmap_b[Face(4)]
+    @test fmap_a[Face(4)] == fmap_b[Face(5)]
 end
 
-contract(a, b, Ia, Ib)
+@testset "contract" begin
+    rzrx = contract(a, b, Ia, Ib)
+    length(rzrx.half_edges_faces) == 42
+end
