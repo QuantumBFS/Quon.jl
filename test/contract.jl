af68fd0e9951fa388d6c885deb54829ecb758ef7
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-03-11 14:56:19 +0800

    fix test

--- test/contract.jl
+++ test/contract.jl
@@ -3,8 +3,8 @@ using Quon
 
 a = planar_rz()
 b = planar_rx()
-Ia = Vertex[8, 7, 6, 5]
-Ib = Vertex[1, 2, 3, 4]
+Ia = Vertex[9, 8, 7, 6, 5, 13]
+Ib = Vertex[9, 1, 2, 3, 4, 13]
 
 @testset "vertices_map" begin
     map_a, map_b = vertices_map(a, b, Ia, Ib)
@@ -15,7 +15,11 @@ end
 
 @testset "faces_map" begin
     map_a, map_b = faces_map(a, b, Ia, Ib)
+    @test map_a[Face(6)] == map_b[Face(1)]
     @test map_a[Face(5)] == map_b[Face(2)]
     @test map_a[Face(2)] == map_b[Face(3)]
-    @test map_a[Face(3)] == map_b[Face(5)]
+    @test map_a[Face(3)] == map_b[Face(4)]
+    @test map_a[Face(4)] == map_b[Face(5)]
 end
+
+contract(a, b, Ia, Ib)
