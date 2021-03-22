7b8f7aac9142872116cc869772d0cc7d5a77bb4f
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-03-22 11:28:27 +0800

    move to planar.jl

--- src/graphs/planar.jl
+++ src/graphs/planar.jl
@@ -1,3 +1,84 @@
+# TODO: check the half edges are in reverse time order
+struct PlanarGraph
+    vertices::Dict{Vertex, HalfEdge}
+    faces_half_edges::Dict{Face, HalfEdge} # face -> edge
+    half_edges_faces::Dict{HalfEdge, Face} # half edge -> face
+    nexts::Dict{HalfEdge, HalfEdge}
+    prevs::Dict{HalfEdge, HalfEdge}
+end
+
+function PlanarGraph(half_edges_faces::Dict{HalfEdge, Face})
+    faces_half_edges = Dict{Face, HalfEdge}()
+    for (he, f) in half_edges_faces
+        haskey(faces_half_edges, f) || (faces_half_edges[f] = he)
+    end
+
+    vertices = Dict{Vertex, HalfEdge}()
+    for he in keys(half_edges_faces)
+        haskey(vertices, he.src) || (vertices[he.src] = he)
+    end
+    return PlanarGraph(vertices, faces_half_edges, half_edges_faces)
+end
+
+function PlanarGraph(vs::Dict{Vertex, HalfEdge}, f2he::Dict{Face, HalfEdge}, he2f::Dict{HalfEdge, Face})
+    hes = keys(he2f)
+    fs = Dict{Face, Vector{HalfEdge}}()
+    for (he, f) in he2f
+        if haskey(fs, f)
+            push!(fs[f], he)
+        else
+            fs[f] = [he]
+        end
+    end
+    nexts = Dict{HalfEdge, HalfEdge}()
+    prevs = Dict{HalfEdge, HalfEdge}()
+    for he in hes
+        f_he = he2f[he]
+        for nhe in fs[f_he]
+            if nhe.src == he.dst
+                nexts[he] = nhe
+                prevs[nhe] = he
+                break
+            end
+        end
+    end
+    return PlanarGraph(vs, f2he, he2f, nexts, prevs)
+end
+
+face(g::PlanarGraph, e::HalfEdge) = g.half_edges_faces[e]
+nv(g::PlanarGraph) = length(keys(g.vertices))
+src(g::PlanarGraph, e::HalfEdge) = src(e)
+dst(g::PlanarGraph, e::HalfEdge) = dst(e)
+twin(g::PlanarGraph, e::HalfEdge) = twin(e)
+
+next(g::PlanarGraph, e::HalfEdge) = g.nexts[e]
+prev(g::PlanarGraph, e::HalfEdge) = g.prevs[e]
+σ(g::PlanarGraph, e::HalfEdge) = twin(g, prev(g, e))
+σ_inv(g::PlanarGraph, e::HalfEdge) = next(g, twin(g, e))
+
+"""
+    half_edges(g)
+
+Returns all half edges in a planar graph `g`.
+"""
+half_edges(g::PlanarGraph) = collect(keys(g.half_edges_faces))
+
+"""
+    edges(g)
+
+Return all whole edges in a planar graph `g`.
+"""
+function edges(g::PlanarGraph)
+    es = Tuple{Vertex, Vertex}[]
+    hes = half_edges(g)
+    for he in hes
+        if twin(he) in hes && !(twin(he) in es)
+            push!(es, (he.src, he.dst))
+        end
+    end
+    return es
+end
+
 """
     planar_rz()
 
