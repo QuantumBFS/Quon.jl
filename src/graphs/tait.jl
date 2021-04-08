e4130e1c1c96e1c0d49b4ee7f168545d23444f28
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-04-08 16:20:21 +0800

    planar multigraphs

--- src/graphs/tait.jl
+++ src/graphs/tait.jl
@@ -2,207 +2,352 @@ mutable struct Phase{T <: Number}
     param::T
     isparallel::Bool
 end
+Phase(p::T, isparallel::Bool) where {T <: Number} = Phase{T}(p, isparallel)
+
 function change_direction!(p::Phase)
     p.param = change_direction(p.param)
     p.isparallel = !p.isparallel
     return p
 end
 
-# src < dst
-struct Edge
-    src::Int
-    dst::Int
-    function Edge(a::Integer, b::Integer)
-        a <= b && return new(a, b)
-        return new(b, a)
+mutable struct QuonTait{P <: Phase}
+    g::PlanarMultigraph
+    phases::Dict{Int, P}    # he_id => phase
+    inputs::Vector{Int}     # list of input vertices
+    outputs::Vector{Int}    # list of output vertices
+    genuses::Set{Int}   # set of vertices on genuses
+end
+
+apis_1 = [:nv, :ne, :nf, :nhe, :vertices, :faces, :half_edges,
+    :check_faces, :check_vertices, :check_combinatorial_maps]
+apis_2 = [:src, :dst, :half_edge, :face,
+    :next, :prev, :twin, :α, :ϕ, :σ, :σ_inv, :is_boundary,
+    :out_half_edge, :surrounding_half_edge,
+    :trace_face, :trace_vertex, :neighbors, :rem_face!
+]
+
+for api in apis_1
+    @eval $api(q::QuonTait) = $api(q.g)
+end
+for api in apis_2
+    @eval $api(q::QuonTait, id) = $api(q.g, id)
+end
+
+function rem_vertex!(q::QuonTait, v::Integer; update::Bool = true)
+    rem_vertex!(q.g, v; update = update)
+    deleteat!(q.inputs, findall(isequal(v), q.inputs))
+    deleteat!(q.outputs, findall(isequal(v), q.outputs))
+    delete!(q.genuses, v)
+
+    return q
+end
+
+function rem_edge!(q::QuonTait, he_id::Integer; update::Bool = true)
+    twin_id = twin(q, he_id)
+    rem_edge!(q, he_id; update = update)
+    delete!(q.phases, he_id)
+    delete!(q.phases, twin_id)
+
+    return q
+end
+
+function merge_graph!(A::QuonTait, B::QuonTait)
+    v_max_A = A.g.v_max
+    he_max_A = A.g.he_max
+    f_max_A = A.g.f_max
+    merge_graph!(A.g, B.g)
+    for (he_id, p) in B.phases
+        A.phases[he_id + he_max_A] = p
     end
-end
-src(e::Edge) = e.src
-dst(e::Edge) = e.dst
-another_end(e::Edge, v::Integer) = (v == e.src) ? e.dst : e.src
-
-mutable struct TaitGraph{P <: Phase}
-    adjlist::Dict{Int, Vector{Int}} # vertex_id => adjacent edge_ids
-    edges::Dict{Int, Edge}  # edge_id => edge
-    phases::Dict{Int, P} # edge_id => phase
-    boundaries::Vector{Int} # list of vertex_id
-    vertex_max::Int
-    edge_max::Int
-end
-
-src(g::TaitGraph, e_id::Integer) = src(g.edges[e_id])
-dst(g::TaitGraph, e_id::Integer) = dst(g.edges[e_id])
-edge(g::TaitGraph, e_id::Integer) = g.edges[e_id]
-adjacent_edges(g::TaitGraph, v_id::Integer) = g.adjlist[v_id]
-neighbors(g::TaitGraph, v_id::Integer) = [another_end(edge(g, e_id), v_id) for e_id in adjacent_edges(g, v_id)]
-
-nv(g::TaitGraph) = length(g.adjlist)
-ne(g::TaitGraph) = length(g.edges)
-
-function rem_vertex!(G::TaitGraph, v::Integer; keep_edge::Bool = false)
-    if !keep_edge
-        adj_v = adjacent_edges(G, v)
-        nb_v = neighbors(G, v)
-
-        for e_id in adj_v
-            u = another_end(edge(G, e_id), v)
-            deleteat!(G.adjlist[u], findall(isequal(e_id), G.adjlist[u]))
-            delete!(G.edges, e_id)
-            delete!(G.phases, e_id)
-        end
+    for v in B.inputs
+        push!(A.inputs, v + v_max_A)
     end
-    delete!(G.adjlist, v)
-    deleteat!(G.boundaries, findall(isequal(v), G.boundaries))
+    for v in B.outputs
+        push!(A.outputs, v + v_max_A)
+    end
+    for v in B.genuses
+        push!(A.genuses, v + v_max_A)
+    end
 
-    return G
+    return A
 end
 
-function rem_edge!(G::TaitGraph, e_id::Integer)
-    s = src(G, e_id)
-    d = dst(G, e_id)
-    delete!(G.edges, e_id)
-    delete!(G.phases, e_id)
-    deleteat!(G.adjlist[s], findall(isequal(e_id), G.adjlist[s]))    
-    deleteat!(G.adjlist[d], findall(isequal(e_id), G.adjlist[d]))    
+Base.copy(q::QuonTait) =
+    QuonTait(copy(q.g), copy(q.phases), copy(q.inputs), copy(q.outputs), copy(q.genuses))
 
-    return G
+phase(q::QuonTait, he_id::Integer) = q.phases[he_id]
+change_direction!(g::QuonTait, e_id::Integer) = change_direction!(g.phases[e_id])
+
+function planar_rz()
+    v2he = Dict{Int, Int}(
+        1 => 1,
+        2 => 9,
+        3 => 4,
+        4 => 6,
+        5 => 14,
+    )
+    half_edges = Dict{Int, HalfEdge}(
+        1 => HalfEdge(1, 2),
+        2 => HalfEdge(2, 1),
+        3 => HalfEdge(1, 3),
+        4 => HalfEdge(3, 1),
+        5 => HalfEdge(1, 4),
+        6 => HalfEdge(4, 1),
+        7 => HalfEdge(2, 3),
+        8 => HalfEdge(3, 2),
+        9 => HalfEdge(2, 5),
+        10 => HalfEdge(5, 2),
+        11 => HalfEdge(3, 5),
+        12 => HalfEdge(5, 3),
+        13 => HalfEdge(4, 5),
+        14 => HalfEdge(5, 4),
+    )
+    
+    f2he = Dict{Int, Int}(
+        0 => 2,
+        1 => 1,
+        2 => 8,
+        3 => 3,
+    )
+    he2f = Dict{Int, Int}(
+        2 => 0,
+        5 => 0,
+        13 => 0,
+        10 => 0,
+
+        1 => 1,
+        7 => 1,
+        4 => 1,
+
+        8 => 2,
+        9 => 2,
+        12 => 2,
+        
+        3 => 3,
+        11 => 3,
+        14 => 3,
+        6 => 3,
+    )    
+    next = Dict{Int, Int}(
+        2 => 5,
+        5 => 13,
+        13 => 10,
+        10 => 2,
+
+        1 => 7,
+        7 => 4,
+        4 => 1,
+
+        8 => 9,
+        9 => 12,
+        12 => 8,
+
+        3 => 11,
+        11 => 14,
+        14 => 6,
+        6 => 3,
+    )
+    twin = Dict{Int, Int}(
+        1 => 2,
+        2 => 1,
+        3 => 4,
+        4 => 3,
+        5 => 6,
+        6 => 5,
+        7 => 8,
+        8 => 7,
+        9 => 10,
+        10 => 9,
+        11 => 12,
+        12 => 11,
+        13 => 14,
+        14 => 13,
+    )
+    rz = PlanarMultigraph(v2he, half_edges,
+        f2he, he2f,
+        next, twin,
+        5, 14, 3
+    )
+    return rz
 end
 
-Base.copy(G::TaitGraph) =
-    TaitGraph(copy(G.adjlist), copy(G.edges), copy(G.phases), copy(G.boundaries), G.vertex_max, G.edge_max)
+function planar_rx()
+    v2he = Dict{Int, Int}(
+        1 => 1,
+        2 => 9,
+        3 => 4,
+        4 => 8,
+        5 => 6,
+        6 => 14,
+    )
+    half_edges = Dict{Int, HalfEdge}(
+        1 => HalfEdge(1, 2),
+        2 => HalfEdge(2, 1),
+        3 => HalfEdge(1, 3),
+        4 => HalfEdge(3, 1),
+        5 => HalfEdge(1, 5),
+        6 => HalfEdge(5, 1),
+        7 => HalfEdge(3, 4),
+        8 => HalfEdge(4, 3),
+        9 => HalfEdge(2, 6),
+        10 => HalfEdge(6, 2),
+        11 => HalfEdge(4, 6),
+        12 => HalfEdge(6, 4),
+        13 => HalfEdge(5, 6),
+        14 => HalfEdge(6, 5),
+    )
+    
+    f2he = Dict{Int, Int}(
+        0 => 2,
+        1 => 1,
+        2 => 3,
+    )
+    he2f = Dict{Int, Int}(
+        2 => 0,
+        5 => 0,
+        13 => 0,
+        10 => 0,
 
-change_direction!(g::TaitGraph, e_id::Integer) = change_direction!(g.phases[e_id])
+        1 => 1,
+        9 => 1,
+        12 => 1,
+        8 => 1,
+        4 => 1,
+
+        3 => 2,
+        7 => 2,
+        11 => 2,
+        14 => 2,
+        6 => 2,
+    )    
+    next = Dict{Int, Int}(
+        2 => 5,
+        5 => 13,
+        13 => 10,
+        10 => 2,
+
+        1 => 9,
+        9 => 12,
+        12 => 8,
+        8 => 4,
+        4 => 1,
+
+        3 => 7,
+        7 => 11,
+        11 => 14,
+        14 => 6,
+        6 => 3,
+    )
+    twin = Dict{Int, Int}(
+        1 => 2,
+        2 => 1,
+        3 => 4,
+        4 => 3,
+        5 => 6,
+        6 => 5,
+        7 => 8,
+        8 => 7,
+        9 => 10,
+        10 => 9,
+        11 => 12,
+        12 => 11,
+        13 => 14,
+        14 => 13,
+    )
+    rx = PlanarMultigraph(v2he, half_edges,
+        f2he, he2f,
+        next, twin,
+        6, 14, 2
+    )
+    return rx
+end
 
 function tait_rx(θ::T) where T
-    adjlist = Dict{Int, Vector{Int}}(
-        1=>[1, 2],
-        2=>[3, 4],
-        3=>[5, 3],
-        4=>[6, 7],
-        5=>[2, 4, 6],
-        6=>[7, 5, 1],
-    )
-    edges = Dict{Int, Edge}(
-        1 => Edge(1, 6),
-        2 => Edge(1, 5),
-        3 => Edge(2, 3),
-        4 => Edge(2, 5),
-        5 => Edge(3, 6),
-        6 => Edge(4, 5),
-        7 => Edge(4, 6),
-    )
+    g = planar_rx()
+    p = Phase(θ, true)
+    phases = Dict(7 => p, 8 => p)
+    inputs = Int[1]
+    outputs = Int[6]
+    genuses = Set([2, 5])
 
-    phases = Dict{Int, Phase{T}}(
-        3 => Phase(θ, true),
-    )
-    boundaries = [5, 6]
-    vmax = 6
-    emax = 7
-    return TaitGraph(adjlist, edges, phases, boundaries, vmax, emax)
+    return QuonTait{Phase{T}}(g, phases, inputs, outputs, genuses)
 end
 
 function tait_rz(θ::T) where T
-    adjlist = Dict{Int, Vector{Int}}(
-        1=>[1, 2],
-        2=>[3, 4, 5],
-        3=>[7, 4, 6],
-        4=>[2, 5, 7],
-        5=>[6, 3, 1],
-    )
-    edges = Dict{Int, Edge}(
-        1 => Edge(1, 5),
-        2 => Edge(1, 4),
-        3 => Edge(2, 5),
-        4 => Edge(2, 3),
-        5 => Edge(2, 4),
-        6 => Edge(3, 5),
-        7 => Edge(3, 4),
-    )
+    g = planar_rz()
+    p = Phase(θ, false)
+    phases = Dict(7 => p, 8 => p)
+    inputs = Int[1]
+    outputs = Int[5]
+    genuses = Set([2, 4])
 
-    phases = Dict{Int, Phase{T}}(
-        4 => Phase(θ, false)
-    )
-    boundaries = [4, 5]
-    vmax = 5
-    emax = 7
-    return TaitGraph(adjlist, edges, phases, boundaries, vmax, emax)
+    return QuonTait{Phase{T}}(g, phases, inputs, outputs, genuses)
 end
 
-function contract!(A::TaitGraph, B::TaitGraph, va::Vector{Int}, vb::Vector{Int})
-    vb .+= A.vertex_max
+function contract!(A::QuonTait, B::QuonTait, va::Vector{Int}, vb::Vector{Int})
     merge_graph!(A, B)
     contract_boundary_vertices!(A, va, vb)
     return A
 end
+contract!(A::QuonTait, B::QuonTait) = contract!(A, B, A.outputs, B.inputs)
+merge_graph(A::QuonTait, B::QuonTait) = merge_graph!(copy(A), B)
 
-merge_graph(A::TaitGraph, B::TaitGraph) = merge_graph!(copy(A), B)
-
-function merge_graph!(A::TaitGraph, B::TaitGraph)
-    for (v_id, e_ids) in B.adjlist
-        A.adjlist[v_id + A.vertex_max] = e_ids .+ A.edge_max
-    end
-
-    for (e_id, e) in B.edges
-        A.edges[e_id + A.edge_max] = Edge(e.src + A.vertex_max, e.dst + A.vertex_max)
-    end
-    for (e_id, p) in B.phases
-        A.phases[e_id + A.edge_max] = p
-    end
-
-    append!(A.boundaries, B.boundaries .+ A.vertex_max)
-    A.vertex_max += B.vertex_max
-    A.edge_max += B.edge_max
-    return A
-end
-
-# function shift_vertex!(g::TaitGraph, n::Integer)
-#     n > 0 || return g
-#     g.vertex_max += n
-#     for (e_id, e) in edges
-#         edges[e_id] = Edge(e.src + n, e.dst + n)
-#     end
-#     new_adjlist = Dict{Int, Vector{Int}}()
-#     for v_id in sort!(collect(keys(g.adjlist)), rev = true)
-#         g.adjlist[v_id + n] = g.adjlist[v_id]
-#         delete!(g.adjlist, v_id)
-#     end
-#     g.boundaries .+= n
-
-#     return g
-# end
-
-function contract_boundary_vertices!(G::TaitGraph, va::Vector{Int}, vb::Vector{Int})
+function contract_boundary_vertices!(q::QuonTait, va::Vector{Int}, vb::Vector{Int})
     for (a, b) in zip(va, vb)
-        adj_a = adjacent_edges(G, a)
-        adj_b = adjacent_edges(G, b)
-        for k in 1:length(adj_a)
-            e_u = adj_a[k]
-            e_v = adj_b[end-k+1]
-            u = another_end(edge(G, e_u), a)
-            v = another_end(edge(G, e_v), b)
-            adj_u = G.adjlist[u]
-            adj_v = G.adjlist[v]
-            shift_adjlist!(adj_u, e_u)
-            shift_adjlist!(adj_v, e_v)
-            append!(adj_u, adj_v)
-            
-            for neighbor_v in G.adjlist[v]
-                e = Edge(u, another_end(edge(G, neighbor_v), v))
-                G.edges[neighbor_v] = e
+        out_a = trace_vertex(q, a)
+        out_b = trace_vertex(q, b)
+        ia = findfirst(he -> is_boundary(q, he), out_a)
+        out_a = [out_a[ia+1:end]; out_a[1:ia]]
+        ib = findfirst(he -> is_boundary(q, he), out_b)
+        out_b = [out_b[ib+1:end]; out_b[1:ib]]
+        reverse!(out_b)
+        dst_a = [dst(q, he) for he in out_a]
+        dst_b = [dst(q, he) for he in out_b]
+        fs_a = [face(q, he) for he in out_a]
+        fs_b = [face(q, he) for he in out_b]
+
+        for k in 1:length(out_a)
+            he_a = out_a[k]
+            he_b = out_b[k]
+            for he_id in trace_vertex(q, dst_b[k])
+                q.g.half_edges[he_id] = HalfEdge(dst_a[k], dst(q, he_id))
+                q.g.half_edges[twin(q, he_id)] = HalfEdge(dst(q, he_id), dst_a[k])
             end
-
-            rem_vertex!(G, v; keep_edge = true)
+            q.g.next[prev(q, twin(q, he_b))] = next(q, he_a)
+            q.g.next[prev(q, twin(q, he_a))] = next(q, he_b)
         end
-        @show (a, b)
-        rem_vertex!(G, a)
-        rem_vertex!(G, b)
+        for k in 1:(length(out_a) - 1)
+            he_a = out_a[k]
+            next_a = next(q, he_a)
+            while src(q, next_a) == a || dst(q, next_a) == a
+                next_a = next_a(q, he_a)
+            end
+            f_a = face(q, next_a)
+            q.g.f2he[f_a] = next_a
+            curr_he = next(q, next_a)
+            while curr_he != next_a
+                q.g.he2f[curr_he] = f_a
+                curr_he = next(q, curr_he)
+            end
+        end
+        for v in dst_b
+            if !(v in dst_a)
+                delete!(q.g.v2he, v)
+                delete!(q.genuses, v)
+            end
+        end
+        for f in fs_b
+            !(f in fs_a) && delete!(q.g.f2he, f)
+        end
+        rem_vertex!(q, a; update = false)
+        rem_vertex!(q, b; update = false)
     end
-    return G
+    return q
 end
 
-function shift_adjlist!(adj, e_id)
-    i_a = findfirst(isequal(e_id), adj)
-    perm = [i_a+1:length(adj)..., 1:i_a...]
-    permute!(adj, perm)
-    return adj
-end
+# function shift_adjlist!(adj, e_id)
+#     i_a = findfirst(isequal(e_id), adj)
+#     perm = [i_a+1:length(adj)..., 1:i_a...]
+#     permute!(adj, perm)
+#     return adj
+# end
---
+++ test/graphs/tait.jl
@@ -0,0 +1,16 @@
+using Quon
+using Test
+
+rx = tait_rx(π)
+trace_vertex(rx, 1)
+rz = tait_rz(π)
+merge_graph!(rz, rx)
+contract_boundary_vertices!(rz, [5], [6])
+@test nv(rz) == 6
+@test ne(rz) == 8
+@test nf(rz) == 4
+
+rz = tait_rz(pi)
+contract_boundary_vertices!(rz, [1], [5])
+vertices(rz)
+rz.g.half_edges
