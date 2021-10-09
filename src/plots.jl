648ce392191d2588d4ca0e58fc1185d258a23f5f
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-10-09 16:52:43 +0800

    add YaoPlots

--- src/plots.jl
+++ src/plots.jl
@@ -1,11 +1,12 @@
+using YaoPlots
 include("plot_compose.jl")
 include("planar_maps.jl")
 
-function plot(q::Tait; backend = :planar, kwargs...)
+function YaoPlots.plot(q::Tait; backend = :planar, kwargs...)
     backend === :compose && return plot_compose(q; kwargs...)
     backend === :planar && return plot_planar(q; kwargs...)
 end
 
-function plot(g::PlanarMultigraph; backend = :planar, kwargs...)
+function YaoPlots.plot(g::PlanarMultigraph; backend = :planar, kwargs...)
     backend === :planar && return plot_planar(g; kwargs...)
 end
