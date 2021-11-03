ee09ad01cadfbe3e282dd170501338fe2793ee90
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-11-03 11:40:12 -0400

    add half edge labels when using :planar_map

--- src/planar_maps.jl
+++ src/planar_maps.jl
@@ -36,7 +36,8 @@ function generate_locs(g::PlanarMultigraph; he_start = nothing)
 end
 
 function plot_graph(vs, es, locs; 
-        vlabel = nothing, vsize = nothing, vfill = nothing, vstroke = nothing,
+        vlabel = nothing, vsize = nothing, vfill = nothing, 
+        vstroke = nothing, vrots = nothing,
         tfill = nothing, tsize = nothing, kwargs...)
     N = length(vs)
     v_map = Dict(zip(vs, 1:N))
@@ -53,14 +54,16 @@ function plot_graph(vs, es, locs;
     vstroke !== nothing && (vstroke = [vstroke[v_map_inv[v]] for v in 1:N])
     tfill !== nothing && (tfill = [tfill[v_map_inv[v]] for v in 1:N])
     tsize !== nothing && (tsize = [tsize[v_map_inv[v]] for v in 1:N])
+    vrots !== nothing && (vrots = [vrots[v_map_inv[v]] for v in 1:N])
     return plot_graph(adjmat, [locs[v_map[v]] for v in vs]; 
-        vlabel = vlabel, vsize = vsize, vfill = vfill, vstroke = vstroke,
+        vlabel = vlabel, vsize = vsize, vfill = vfill, vstroke = vstroke, vrots = vrots,
         tfill = tfill, tsize = tsize, kwargs...)
 end
 
 function plot_graph(adjmat, locs; padding = 0.2, 
-        vlabel = nothing, vsize = nothing, vfill = nothing, vstroke = nothing,
-        tfill = nothing, tsize = nothing,
+        vlabel = nothing, vsize = nothing, vfill = nothing, 
+        vstroke = nothing, vrots = nothing,
+        tfill = nothing, tsize = nothing, 
         layout = nothing)
     layout === :spring && (locs = Spring(; initialpos = locs, C = 0.2)(adjmat))
     layout === :stress && (locs = Stress(; initialpos = locs)(adjmat))
@@ -79,6 +82,7 @@ function plot_graph(adjmat, locs; padding = 0.2,
     vstroke === nothing && (vstroke = ["black" for _ in 1:N])
     tfill === nothing && (tfill = ["red" for _ = 1:N])
     tsize === nothing && (tsize = [8pt for _ = 1:N])
+    vrots === nothing && (vrots = [0.0 for v in 1:N])
     line_locs = []
     for i = 1:N
         for j = i:N
@@ -87,7 +91,7 @@ function plot_graph(adjmat, locs; padding = 0.2,
     end
     
     return compose(context(units=UnitBox(x_min-padding, y_min-padding, x_max-x_min+2padding, y_max-y_min+2padding)),
-        ((context(), text(x_locs[v], y_locs[v], vlabel[v], hcenter, vcenter), 
+        ((context(), text(x_locs[v], y_locs[v], vlabel[v], hcenter, vcenter, Rotation(vrots[v], x_locs[v], y_locs[v])), 
             fill(tfill[v]), fontsize(tsize[v])) for v in 1:N)...,
         (context(), circle(x_locs, y_locs, vsize), 
             fill(vfill), stroke(vstroke)),
@@ -122,6 +126,7 @@ function plot_planar(q::Tait; scale = 1, kwargs...)
     vsize = Dict()
     vfill = Dict()
     vstroke = Dict()
+    vrots = Dict()
     tfill = Dict()
     tsize = Dict()
     for v in vertices(q)
@@ -129,6 +134,7 @@ function plot_planar(q::Tait; scale = 1, kwargs...)
         vsize[v_map[v]] = is_open_vertex(q, v) ? 0.06 : 0.04
         vfill[v_map[v]] = is_open_vertex(q, v) ? "white" : (is_genus(q, v) ? "white" : "royalblue")
         vstroke[v_map[v]] = is_open_vertex(q, v) ? "white" : "royalblue"
+        vrots[v_map[v]] = 0.0
         tfill[v_map[v]] = "red"
         tsize[v_map[v]] = 8/scale*pt
     end
@@ -141,16 +147,21 @@ function plot_planar(q::Tait; scale = 1, kwargs...)
     end
     self_loops = Set{Int}()
     for (he, v) in hes_map
-        if src(q, he) != dst(q, he)
+        if src(q, he) != dst(q, he) # not a self-loop
             locs[v] = (he_locs[he][1]*scale, he_locs[he][2]*scale)
-            vfill[v] = "gray"
-            vstroke[v] = "gray"
+            v_src = v_map[src(q, he)]
+            v_dst = v_map[dst(q, he)]
+            vrots[v] = angle((locs[v_src][1] - locs[v_dst][1]) - im * (locs[v_src][2] - locs[v_dst][2]))
             if is_open_half_edge(q, he)
-                vsize[v] = 0
-                vlabel[v] = ""
-            else
+                vfill[v] = "transparent"
+                vstroke[v] = "transparent"
                 vsize[v] = 0.04/scale
-                vlabel[v] = "$(phase(q, he))"
+                vlabel[v] = "-$he→\n←$(twin(q, he))-"
+            else
+                vfill[v] = "gray"
+                vstroke[v] = "gray"
+                vsize[v] = 0.04/scale
+                vlabel[v] = "-$he→\n$(phase(q, he))\n←$(twin(q, he))-"
             end
             tfill[v] = "black"
             tsize[v] = 5/scale*pt
@@ -169,14 +180,24 @@ function plot_planar(q::Tait; scale = 1, kwargs...)
             vfill[v2] = "gray"
             vstroke[v1] = "gray"
             vstroke[v2] = "gray"
+            vrots[v1] = angle((locs[v1][1] - locs[v2][1]) - im * (locs[v1][2] - locs[v2][2]))
+            vrots[v2] = 0.0
             if is_open_half_edge(q, he1)
-                vsize[v1] = 0
-                vsize[v2] = 0
-                vlabel[v1] = ""
+                vfill[v1] = "transparent"
+                vfill[v2] = "transparent"
+                vstroke[v1] = "transparent"
+                vstroke[v2] = "transparent"
+                vsize[v1] = 0.04/scale
+                vsize[v2] = 0.04/scale
+                vlabel[v1] = "-$he→\n←$(twin(q, he))-"
                 vlabel[v2] = ""
             else
+                vfill[v1] = "gray"
+                vfill[v2] = "gray"
+                vstroke[v1] = "gray"
+                vstroke[v2] = "gray"
                 vsize[v1] = 0.04/scale
-                vlabel[v1] = "$(phase(q, he))"
+                vlabel[v1] = "-$he→\n$(phase(q, he))\n←$(twin(q, he))-"
                 vsize[v2] = 0
                 vlabel[v2] = ""
             end
@@ -191,7 +212,7 @@ function plot_planar(q::Tait; scale = 1, kwargs...)
     v_max = length(new_vs)
     for he in half_edges(q)
         (he in hes_recorded || twin(q, he) in hes_recorded) && continue
-        
+        push!(hes_recorded, he)
         v_max += 1
         push!(new_vs, v_max)
         v1 = v_map[src(q, he)]
@@ -201,18 +222,22 @@ function plot_planar(q::Tait; scale = 1, kwargs...)
         locs[v_max] = ((locs[v1][1] + locs[v2][1])/2, (locs[v1][2] + locs[v2][2])/2)
         tfill[v_max] = "black"
         tsize[v_max] = 5/scale*pt
-        vfill[v_max] = "gray"
-        vstroke[v_max] = "gray"
+        vrots[v_max] = angle((locs[v1][1] - locs[v2][1]) - im * (locs[v1][2] - locs[v2][2]))
         if is_open_half_edge(q, he)
-            vsize[v_max] = 0.0
-            vlabel[v_max] = ""
-        else
+            vfill[v_max] = "transparent"
+            vstroke[v_max] = "transparent"
             vsize[v_max] = 0.04/scale
-            vlabel[v_max] = "$(phase(q, he))"
+            vlabel[v_max] = "-$(he)→\n←$(twin(q, he))-"
+        else
+            vfill[v_max] = "gray"
+            vstroke[v_max] = "gray"
+            vsize[v_max] = 0.04/scale
+            vlabel[v_max] = "-$(he)→\n$(phase(q, he))\n←$(twin(q, he))-"
         end
     end
 
     return plot_graph(new_vs, new_es, locs; 
-        vlabel = vlabel, vsize = vsize, vfill = vfill, vstroke = vstroke,
+        vlabel = vlabel, vsize = vsize, vfill = vfill, 
+        vstroke = vstroke, vrots = vrots,
         tfill = tfill, tsize = tsize, kwargs...)
 end
