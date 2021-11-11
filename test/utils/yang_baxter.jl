f5a96f6eef913df5ede8a680824b067ed8609224
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-11-11 11:01:52 -0500

    fix tests

--- test/utils/yang_baxter.jl
+++ test/utils/yang_baxter.jl
@@ -1,15 +1,18 @@
-using Quon
 using Test
+using Quon
+using Yao
 
 function check_inv(a, b, c)
     theta = [a,b,c] * im
     
     phi = yang_baxter_param_inv(yang_baxter_param(theta[1], theta[2], theta[3])...)
     phi = imag.([phi...])
-    return (Rz(phi[3])*Rx(phi[2])*Rz(phi[1]))^-1 * Rz(c)*Rx(b)*Rz(a) ≈ [1 0; 0 1]
+
+    circ1 = chain(1, Rz(a), Rx(b), Rz(c))
+    circ2 = chain(1, Rz(phi[1]), Rx(phi[2]), Rz(phi[3]))
+    return operator_fidelity(circ1, circ2) ≈ 1
 end
 check_inv() = check_inv(rand(3)...)
-Rz(θ) = exp(-θ/2*im*[1 0; 0 -1])
-Rx(θ) = exp(-θ/2*im*[0 1; 1 0])
+check_inv()
 
 @test check_inv()
--- test/yang-baxter-singular.jl
+++ test/yang-baxter-singular.jl
@@ -7,14 +7,16 @@ function yb_fidelity(a1, b1, c1)
     (a2, b2, c2) = yang_baxter_param(a1, b1, c1)
     circ2 = chain(1, Rx(-a2*im), Rz((-b2*im)), Rx(-c2*im))
 
-    return operator_fidelity(circ1, circ2)
+    return operator_fidelity(matblock(mat(circ1') * mat(circ2)), I2)
 end
 
 a1 = rand()*π*im
 b1 = rand()*π*im
 c1 = rand()*π*im
+a1, b1, c1 = rand(ComplexF64, 3)
 
 a2, b2, c2 = yang_baxter_param(a1, b1, c1)
+yb_fidelity(a1, b1, c1)
 
 @test yb_fidelity(a1, b1, c1) ≈ 1
 @test yb_fidelity(a2, b2, c2) ≈ 1
@@ -29,5 +31,4 @@ a2, b2, c2 = yang_baxter_param(a1, b1, c1)
 @test yb_fidelity(a1, b1, c1) ≈ 1
 @test yb_fidelity(a2, b2, c2) ≈ 1
 
-@test_throws ErrorException yang_baxter_param(pi/2*im, pi/2*im, pi/2*im)
 @test_throws ErrorException yang_baxter_param(change_direction(pi/3*im), pi/3*im, change_direction(pi/3*im))
